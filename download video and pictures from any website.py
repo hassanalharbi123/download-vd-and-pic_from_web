bb41b2f79a1254f5b8ba0ac1ca699230f364c60b
Alharb7 <76629405+hassanalharbi123@users.noreply.github.com>
2021-01-24 13:26:15 +0300

    Update download video and pictures from any website.py

--- download video and pictures from any website.py
+++ download video and pictures from any website.py
@@ -1,9 +1,12 @@
-print(""" download picture [1] 
- download video [2]""")
-op=input("choose the options \n")
+print("------------ created by alhassan alharbi ------------")
+
+print("\033[0;37m twitter ===>\033[3;34m @alhassanAlharb7 \033[0;37m<=== ")
+print("""\033[0;32m download picture [1]  
+ download video [2]\033[1;37m""")
+op=input("\033[3;36m choose the options \n")
 if op=="1":
     import requests
-    r=requests.get(input("enter the link for picture >> >> "))
+    r=requests.get(input("\033[0;31m enter the link for picture >> >> "))
     a=input("enter the file name >>>>")
     with open(a+'.png','wb')as f:
         f.write(r.content)
@@ -15,4 +18,3 @@ if op=="2":
     with open(input("enter the name foe video ==> ")+".mp4",'wb') as f:
         for video in r.iter_content(chunk_size=chunk_size):
             f.write(video)
-
